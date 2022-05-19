1276e3f19f79e6931fc0c03cfd2ef06aa47141a4
StanHunter <Stasdolg@mail.ru>
2022-05-20 03:25:10 +0700

    Add Ability System To Player

--- Source/GAS_Gideon/Private/GASCharacter.cpp
+++ Source/GAS_Gideon/Private/GASCharacter.cpp
@@ -1,34 +1,195 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "GASCharacter.h"
+#include "GASAbilitySystemComponent.h"
+#include "GASAttributeSet.h"
 
-// Sets default values
 AGASCharacter::AGASCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-}
+	AbilitySystemComponent = CreateDefaultSubobject<UGASAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
 
-// Called when the game starts or when spawned
-void AGASCharacter::BeginPlay()
-{
-	Super::BeginPlay();
 	
 }
 
-// Called every frame
+void AGASCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (AbilitySystemComponent)
+	{
+		AttributeSetVar = AbilitySystemComponent->GetSet<UGASAttributeSet>();
+
+		const_cast<UGASAttributeSet*>(AttributeSetVar)->HealthChangeDelegate.AddDynamic(this, &AGASCharacter::OnHealthChangedNative);
+		const_cast<UGASAttributeSet*>(AttributeSetVar)->ManaChangeDelegate.AddDynamic(this, &AGASCharacter::OnManaChangedNative);
+		const_cast<UGASAttributeSet*>(AttributeSetVar)->BaseAttackDamagedChangeDelegate.AddDynamic(this, &AGASCharacter::OnBaseAttackDamagedChangedNative);
+		const_cast<UGASAttributeSet*>(AttributeSetVar)->SpeedMultiplierChangeDelegate.AddDynamic(this, &AGASCharacter::OnSpeedMultiplierChangeNative);
+		
+		//Alternate Method for Attribute Change Delegate
+		//AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AttributeSetVar->GetHealthAttribute()).AddUObject(this, &AGASCharacter::OnHealthChangedNative);
+		//AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AttributeSetVar->GetManaAttribute()).AddUObject(this, &AGASCharacter::OnManaChangedNative);
+	}
+}
+
 void AGASCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-// Called to bind functionality to input
 void AGASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+UAbilitySystemComponent* AGASCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+void AGASCharacter::OnHealthChangedNative(float Health, int32 StackCount)
+{
+	OnHealthChanged(Health, StackCount);
+}
+
+void AGASCharacter::OnManaChangedNative(float Mana, int32 StackCount)
+{
+	OnManaChanged(Mana, StackCount);
+}
+
+void AGASCharacter::OnBaseAttackDamagedChangedNative(float BaseAttackDamage, int32 StackCount)
+{
+	OnBaseAttackDamagedChanged(BaseAttackDamage, StackCount);
+}
+
+void AGASCharacter::OnSpeedMultiplierChangeNative(float SpeedMultiplier, int32 StackCount)
+{
+	OnSpeedMultierChange(SpeedMultiplier, StackCount);
+}
+
+//Function Definition for Alternate Method for Attribute Change Delegates
+//void AGASCharacter::OnHealthChangedNative(const FOnAttributeChangeData& Data)
+//{
+//	OnHealthChanged(Data.OldValue, Data.NewValue);
+//}
+//
+//void AGASCharacter::OnManaChangedNative(const FOnAttributeChangeData& Data)
+//{
+//	OnManaChanged(Data.OldValue, Data.NewValue);
+//}
+
+
+void AGASCharacter::InitializeAbility(TSubclassOf<UGameplayAbility> AbilityToGet, int32 AbilityLevel)
+{
+	if (HasAuthority() && AbilitySystemComponent)
+	{
+		AbilitySystemComponent->GiveAbility(FGameplayAbilitySpec(AbilityToGet, AbilityLevel, 0));
+	}
+}
+
+void AGASCharacter::InitializeAbilityMulti(TArray<TSubclassOf<UGameplayAbility>> AbilitiesToAcquire, int32 AbilityLevel)
+{
+	for (const auto AbilityItem : AbilitiesToAcquire)
+	{
+		InitializeAbility(AbilityItem, AbilityLevel);
+	}
+}
+
+void AGASCharacter::InitializeAbilityUsingDictionary(TMap<TSubclassOf<UGameplayAbility>, int32> Abilities)
+{
+	for (const auto Item : Abilities)
+	{
+		InitializeAbility(Item.Key, Item.Value);
+	}
+}
+
+void AGASCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+
+	AbilitySystemComponent->InitAbilityActorInfo(this,this);
+	
+	//InitializeAbilityMulti(InitialAbilitiesList, 0);
+	InitializeAbilityUsingDictionary(InitialAbilitiesDict);
+}
+
+void AGASCharacter::OnRep_PlayerState()
+{
+	Super::OnRep_PlayerState();
+	AbilitySystemComponent->InitAbilityActorInfo(this, this);
+}
+
+void AGASCharacter::GetHealthValues(float& Health, float& MaxHealth) const
+{
+	if (AttributeSetVar)
+	{
+		Health = AttributeSetVar->GetHealth();
+		MaxHealth = AttributeSetVar->GetMaxHealth();
+	}
+}
+
+void AGASCharacter::GetManaValues(float& Mana, float& MaxMana) const
+{
+	if (AttributeSetVar)
+	{
+		Mana = AttributeSetVar->GetMana();
+		MaxMana = AttributeSetVar->GetMaxMana();
+	}
+}
+
+void AGASCharacter::GetBaseAttackDamageValue(float& BaseAttackDamage)
+{
+	if (AttributeSetVar)
+	{
+		BaseAttackDamage = AttributeSetVar->GetBaseAttackDamage();
+	}
+}
+
+void AGASCharacter::GetSpeedMultiplierValue(float& SpeedMultiplier)
+{
+	SpeedMultiplier = AttributeSetVar->GetSpeedMultiplier();
+}
+
+void AGASCharacter::RemoveAbilityWithTags(FGameplayTagContainer TagContainer)
+{
+	TArray<FGameplayAbilitySpec*> MatchingAbilities;
+
+	AbilitySystemComponent->GetActivatableGameplayAbilitySpecsByAllMatchingTags(TagContainer, MatchingAbilities, true);
+
+	for (const auto Spec : MatchingAbilities)
+	{
+		AbilitySystemComponent->ClearAbility(Spec->Handle);
+	}
+}
+
+void AGASCharacter::CancelAbilityWithTags(FGameplayTagContainer WithTags, FGameplayTagContainer WithOutTags)
+{
+	AbilitySystemComponent->CancelAbilities(&WithTags, &WithOutTags, nullptr);
+}
+
+void AGASCharacter::ChangeAbilityLevelWithTags(FGameplayTagContainer TagContainer, int32 NewLevel)
+{
+	TArray<FGameplayAbilitySpec*> MatchingAbilities;
+	AbilitySystemComponent->GetActivatableGameplayAbilitySpecsByAllMatchingTags(TagContainer, MatchingAbilities, true);
+
+	for (const auto Spec : MatchingAbilities)
+	{
+		Spec->Level = NewLevel;
+	}
+}
+
+void AGASCharacter::AddLooseGameplayTag(FGameplayTag TagToAdd)
+{
+	GetAbilitySystemComponent()->AddLooseGameplayTag(TagToAdd);
+	GetAbilitySystemComponent()->SetTagMapCount(TagToAdd, 1);
+}
+
+void AGASCharacter::RemoveLooseGameplayTag(FGameplayTag TagToRemove)
+{
+	GetAbilitySystemComponent()->RemoveLooseGameplayTag(TagToRemove);
 }
 
--- Source/GAS_Gideon/Public/GASCharacter.h
+++ Source/GAS_Gideon/Public/GASCharacter.h
@@ -1,29 +1,129 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
+#include "GameplayEffectTypes.h"
 #include "GASCharacter.generated.h"
 
+class UGASAbilitySystemComponent;
+class UGASAttributeSet;
+
 UCLASS()
-class GAS_GIDEON_API AGASCharacter : public ACharacter
+class GAS_GIDEON_API AGASCharacter : public ACharacter, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	AGASCharacter();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
+	
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="GASGameplayAbility")
+	class UGASAbilitySystemComponent* AbilitySystemComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="GASGameplayAbility")
+	const class UGASAttributeSet* AttributeSetVar;
+
+	//UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="GASGameplayAbility")
+	//TArray<TSubclassOf<class UGameplayAbility>> InitialAbilitiesList;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="GASGameplayAbility")
+	TMap<TSubclassOf<class UGameplayAbility>, int32> InitialAbilitiesDict;
+	
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+	// Native
+	
+	UFUNCTION()
+	virtual void OnHealthChangedNative(float	Health, int32 StackCount);
+
+	UFUNCTION()
+	virtual void OnManaChangedNative(float Mana, int32 StackCount);
+
+	UFUNCTION()
+	virtual void OnBaseAttackDamagedChangedNative(float	BaseAttackDamage, int32 StackCount);
+
+	UFUNCTION()
+	virtual void OnSpeedMultiplierChangeNative(float SpeedMultiplier, int32 StackCount);
+
+	// BP
+
+	UFUNCTION(BlueprintImplementableEvent, Category="GASGameplayAbility")
+	void OnHealthChanged(float	Health, int32 StackCount);
+
+	UFUNCTION(BlueprintImplementableEvent, Category="GASGameplayAbility")
+	void OnManaChanged(float Mana, int32 StackCount);
+
+	UFUNCTION(BlueprintImplementableEvent, Category="GASGameplayAbility")
+	void OnBaseAttackDamagedChanged(float	BaseAttackDamage, int32 StackCount);
+
+	UFUNCTION(BlueprintImplementableEvent, Category="GASGameplayAbility")
+	void OnSpeedMultierChange(float SpeedMultiplier, int32 StackCount);
+
+	//Alternate Method for Attribute Change Delagates
+	////Native functions for Health Change and Mana Change
+	//virtual void OnHealthChangedNative(const FOnAttributeChangeData& Data);
+	//virtual void OnManaChangedNative(const FOnAttributeChangeData& Data);
+
+	////Called when Health is Changed in BP
+	// UFUNCTION(BlueprintImplementableEvent, Category = "GASGameplayAbility")
+	//void OnHealthChanged(float OldValue, float NewValue);
+	//
+	//////Called when Mana is Changed in BP
+	// UFUNCTION(BlueprintImplementableEvent, Category = "GASGameplayAbility")
+	//void OnManaChanged(float OldValue, float NewValue);
+
+	// Init Ability
+
+	UFUNCTION(BlueprintCallable, Category="GASGameplayAbility")
+	void InitializeAbility(TSubclassOf<UGameplayAbility> AbilityToGet, int32 AbilityLevel);
+
+	UFUNCTION(BlueprintCallable, Category="GASGameplayAbility")
+	void InitializeAbilityMulti(TArray<TSubclassOf<UGameplayAbility>> AbilitiesToAcquire, int32 AbilityLevel);
+
+	UFUNCTION(BlueprintCallable, Category="GASGameplayAbility")
+	void InitializeAbilityUsingDictionary(TMap<TSubclassOf<UGameplayAbility>, int32> Abilities);
+
+	// 
+	virtual void PossessedBy(AController* NewController) override;
+	virtual void OnRep_PlayerState() override;
+
+	// ======================================== Getters ========================================
+
+	UFUNCTION(BlueprintPure, Category="GASGameplayAbility")
+	void GetHealthValues(float& Health, float& MaxHealth) const;
+
+	UFUNCTION(BlueprintPure, Category="GASGameplayAbility")
+	void GetManaValues(float& Mana, float& MaxMana) const;
+
+	UFUNCTION(BlueprintPure, Category="GASGameplayAbility")
+	void GetBaseAttackDamageValue(float& BaseAttackDamage);
+
+	UFUNCTION(BlueprintPure, Category="GASGameplayAbility")
+	void GetSpeedMultiplierValue(float& SpeedMultiplier);
+
+	// ======================================== Ability Modifier Functions ========================================
+
+	UFUNCTION(BlueprintCallable, Category="GASGameplayAbility")
+	void RemoveAbilityWithTags(FGameplayTagContainer TagContainer);
+
+	UFUNCTION(BlueprintCallable, Category="GASGameplayAbility")
+	void CancelAbilityWithTags(FGameplayTagContainer WithTags, FGameplayTagContainer WithOutTags);
+
+	UFUNCTION(BlueprintCallable, Category="GASGameplayAbility")
+	void ChangeAbilityLevelWithTags(FGameplayTagContainer TagContainer, int32 NewLevel);
+	
+	UFUNCTION(BlueprintCallable, Category="GASGameplayAbility")
+	void AddLooseGameplayTag(FGameplayTag TagToAdd);
+
+	UFUNCTION(BlueprintCallable, Category="GASGameplayAbility")
+	void RemoveLooseGameplayTag(FGameplayTag TagToRemove);
+	
 };
